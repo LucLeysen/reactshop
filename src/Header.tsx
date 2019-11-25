241fcb9d2f16eb4f037de39bb8ec07265a0c11f1
Luc L <luc56.leysen@gmail.com>
2019-11-25 15:59:32 +0100

    React routing

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,23 +1,58 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, RouteComponentProps, withRouter } from "react-router-dom";
+import "url-search-params-polyfill";
 
 import logo from "./logo.svg";
 
-const Header: React.SFC = () => {
+const Header: React.SFC<RouteComponentProps> = props => {
+  const [search, setSearch] = React.useState("");
+
+  React.useEffect(() => {
+    const searchParams = new URLSearchParams(props.location.search);
+    setSearch(searchParams.get("search") || "");
+  }, []);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.currentTarget.value);
+  };
+
+  const handleSearchKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      props.history.push(`/products?search=${search}`);
+    }
+  };
+
   return (
     <header className="header">
+      <div className="search-container">
+        <input
+          type="search"
+          placeholder="search"
+          value={search}
+          onChange={handleSearchChange}
+          onKeyDown={handleSearchKeyDown}
+        />
+      </div>
       <img src={logo} className="header-logo" alt="logo" />
       <h1 className="header-title">React Shop</h1>
       <nav>
-        <Link to="/products" className="header-link">
+        <NavLink
+          to="/products"
+          className="header-link"
+          activeClassName="header-link-active"
+        >
           Products
-        </Link>
-        <Link to="/admin" className="header-link">
+        </NavLink>
+        <NavLink
+          to="/admin"
+          className="header-link"
+          activeClassName="header-link-active"
+        >
           Admin
-        </Link>
+        </NavLink>
       </nav>
     </header>
   );
 };
 
-export default Header;
+export default withRouter(Header);
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,34 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
+import ProductPage from "./ProductPage";
+import NotFoundPage from "./NotFoundPage";
 
 import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import Header from "./Header";
+import LoginPage from "./LoginPage";
 
 const Routes: React.SFC = () => {
+  const [loggedIn, setLoggedIn] = React.useState(true);
   return (
     <Router>
       <div>
         <Header />
-        <Route path="/products" component={ProductsPage} />
-        <Route path="/admin" component={AdminPage} />
+        <Switch>
+          <Redirect exact={true} from="/" to="/products" />
+          <Route exact={true} path="/products" component={ProductsPage} />
+          <Route path="/products/:id" component={ProductPage} />
+          <Route path="/admin">
+            {loggedIn ? <AdminPage /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/login" component={LoginPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     </Router>
   );
